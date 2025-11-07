33601bbd2a9c75a5c07cd776e0b7f7323ad3d31b
Bilal Çavuş <bilalcavus01@gmail.com>
2025-11-07 17:03:22 +0300

    app icon updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
         applicationId = "com.bilalcavus.farmodo"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 26
+        versionCode = 27
         versionName = "1.5.6"
     }
 
